535cda9a9f82b5a36a29f5c62ad3c2d8fed96a41
Dave <david.roig@ikasle.egibide.org>
2017-11-20 20:04:10 +0100

    Ejercicio 10 rev2

--- 10_morse/src/com/company/Main.java
+++ 10_morse/src/com/company/Main.java
@@ -7,146 +7,148 @@ import java.io.InputStreamReader;
 public class Main {
 
     private static void morse(String texto) {
-        int length = texto.length();
-        texto += "   "; //hecho para evitar out of bounds errors
-        for (int i = 0; i < length; i++) {
-            if (texto.substring(i, i + 2).equals("ch")) {
-                System.out.print("— — — —");
-                i++;
-            } else
-                switch (texto.substring(i, i + 1)) {
-                    case " ":
-                        break;
-                    case "a":
-                        System.out.print("· —");
-                        break;
-                    case "b":
-                        System.out.print("— · · ·");
-                        break;
-                    case "c":
-                        System.out.print("— · — ·");
-                        break;
-                    case "d":
-                        System.out.print("— · ·");
-                        break;
-                    case "e":
-                        System.out.print("— · ·");
-                        break;
-                    case "f":
-                        System.out.print("· · — ·");
-                        break;
-                    case "g":
-                        System.out.print("— — ·");
-                        break;
-                    case "h":
-                        System.out.print("· · · ·");
-                        break;
-                    case "i":
-                        System.out.print("· ·");
-                        break;
-                    case "j":
-                        System.out.print("· — — —");
-                        break;
-                    case "k":
-                        System.out.print("· — — —");
-                        break;
-                    case "l":
-                        System.out.print("· — · ·");
-                        break;
-                    case "m":
-                        System.out.print("— —");
-                        break;
-                    case "n":
-                        System.out.print("— ·");
-                        break;
-                    case "ñ":
-                        System.out.print("— — · — —");
-                        break;
-                    case "o":
-                        System.out.print("— — —");
-                        break;
-                    case "p":
-                        System.out.print("· — — ·");
-                        break;
-                    case "q":
-                        System.out.print("— — · —");
-                        break;
-                    case "r":
-                        System.out.print("· — ·");
-                        break;
-                    case "s":
-                        System.out.print("· · ·");
-                        break;
-                    case "t":
-                        System.out.print("—");
-                        break;
-                    case "u":
-                        System.out.print("· · —");
-                        break;
-                    case "v":
-                        System.out.print("· · · —");
-                        break;
-                    case "w":
-                        System.out.print("· — —");
-                        break;
-                    case "x":
-                        System.out.print("— · · —");
-                        break;
-                    case "y":
-                        System.out.print("— · — —");
-                        break;
-                    case "z":
-                        System.out.print("— — · ·");
-                        break;
-                    case "0":
-                        System.out.print("— — — — —");
-                        break;
-                    case "1":
-                        System.out.print("· — — — —");
-                        break;
-                    case "2":
-                        System.out.print("· · — — —");
-                        break;
-                    case "3":
-                        System.out.print("· · · — —");
-                        break;
-                    case "4":
-                        System.out.print("· · · · —");
-                        break;
-                    case "5":
-                        System.out.print("· · · · ·");
-                        break;
-                    case "6":
-                        System.out.print("— · · · ·");
-                        break;
-                    case "7":
-                        System.out.print("— — · · ·");
-                        break;
-                    case "8":
-                        System.out.print("— — — · ·");
-                        break;
-                    case "9":
-                        System.out.print("— — — — ·");
-                        break;
-                    case ".":
-                        System.out.print("· — · — · —");
-                        break;
-                    case ",":
-                        System.out.print("— · — · — —");
-                        break;
-                    case "?":
-                        System.out.print("· · — — · ·");
-                        break;
-                    case "\"":
-                        System.out.print("· — · · — ·");
-                        break;
-                    case "!":
-                        System.out.print("— — · · — —");
-                        break;
-                    default:
-                        System.out.print("?");
-                        break;
-                }
+
+        for (int i = 0; i < texto.length(); i++) {
+            switch (texto.substring(i, i + 1)) {
+                case " ":
+                    break;
+                case "a":
+                    System.out.print("· —");
+                    break;
+                case "b":
+                    System.out.print("— · · ·");
+                    break;
+                case "c":
+                    if (i + 1 != texto.length()) {
+                        if (texto.substring(i, i + 2).equals("ch")) {
+                            System.out.print("— — — —");
+                            i++;
+                            break;
+                        }
+                    }
+                    System.out.print("— · — ·");
+                    break;
+                case "d":
+                    System.out.print("— · ·");
+                    break;
+                case "e":
+                    System.out.print("— · ·");
+                    break;
+                case "f":
+                    System.out.print("· · — ·");
+                    break;
+                case "g":
+                    System.out.print("— — ·");
+                    break;
+                case "h":
+                    System.out.print("· · · ·");
+                    break;
+                case "i":
+                    System.out.print("· ·");
+                    break;
+                case "j":
+                    System.out.print("· — — —");
+                    break;
+                case "k":
+                    System.out.print("· — — —");
+                    break;
+                case "l":
+                    System.out.print("· — · ·");
+                    break;
+                case "m":
+                    System.out.print("— —");
+                    break;
+                case "n":
+                    System.out.print("— ·");
+                    break;
+                case "ñ":
+                    System.out.print("— — · — —");
+                    break;
+                case "o":
+                    System.out.print("— — —");
+                    break;
+                case "p":
+                    System.out.print("· — — ·");
+                    break;
+                case "q":
+                    System.out.print("— — · —");
+                    break;
+                case "r":
+                    System.out.print("· — ·");
+                    break;
+                case "s":
+                    System.out.print("· · ·");
+                    break;
+                case "t":
+                    System.out.print("—");
+                    break;
+                case "u":
+                    System.out.print("· · —");
+                    break;
+                case "v":
+                    System.out.print("· · · —");
+                    break;
+                case "w":
+                    System.out.print("· — —");
+                    break;
+                case "x":
+                    System.out.print("— · · —");
+                    break;
+                case "y":
+                    System.out.print("— · — —");
+                    break;
+                case "z":
+                    System.out.print("— — · ·");
+                    break;
+                case "0":
+                    System.out.print("— — — — —");
+                    break;
+                case "1":
+                    System.out.print("· — — — —");
+                    break;
+                case "2":
+                    System.out.print("· · — — —");
+                    break;
+                case "3":
+                    System.out.print("· · · — —");
+                    break;
+                case "4":
+                    System.out.print("· · · · —");
+                    break;
+                case "5":
+                    System.out.print("· · · · ·");
+                    break;
+                case "6":
+                    System.out.print("— · · · ·");
+                    break;
+                case "7":
+                    System.out.print("— — · · ·");
+                    break;
+                case "8":
+                    System.out.print("— — — · ·");
+                    break;
+                case "9":
+                    System.out.print("— — — — ·");
+                    break;
+                case ".":
+                    System.out.print("· — · — · —");
+                    break;
+                case ",":
+                    System.out.print("— · — · — —");
+                    break;
+                case "?":
+                    System.out.print("· · — — · ·");
+                    break;
+                case "\"":
+                    System.out.print("· — · · — ·");
+                    break;
+                case "!":
+                    System.out.print("— — · · — —");
+                    break;
+                default:
+                    System.out.print("?");
+                    break;
+            }
             System.out.print(" ");
         }
         System.out.println();
